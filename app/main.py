514d732e08a567e44e4d132cdddd133f8240ded4
giraljv <sebastiangiraldo2612@gmail.com>
2022-11-30 17:30:59 -0500

    se cambio codigo para pandas

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('data.csv')
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  # data = read_csv.read_csv('data.csv')
+  # data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
+  # countries = list(map(lambda x: x['Country'], data))
+  # percentages = list(map(lambda x: x['World Population Percentage'], data))
   
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent']=='Africa']
+  countries = df['Country'].values
   country = input('Type Country => ')
+  percentage = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentage)
+  data = read_csv.read_csv('data.csv')
 
   result = utils.population_by_country(data, country)
 
